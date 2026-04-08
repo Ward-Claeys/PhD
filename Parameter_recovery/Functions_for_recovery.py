a6b9c9a3a370a0c99dcadc2feff740c4fb0963cf
Ward-Claeys <Ward.Claeys@UGent.be>
2026-04-08 16:53:32 +0200

    Update

--- Parameter_recovery/Functions_for_recovery.py
+++ Parameter_recovery/Functions_for_recovery.py
@@ -11,6 +11,7 @@ import os, sys
 import numpy as np
 import pandas as pd
 from scipy import optimize
+from Parameter_recovery import generate_dataset
 
 #Avoid warnings
 import warnings
@@ -18,11 +19,15 @@ warnings.filterwarnings('ignore')
 
 os.chdir("/Users/wardclaeys/Documents/Github/PhD/Parameter_recovery")
 
-def softmax(values = np.array([0.5, 0.5 , 0.5]) , PEs = np.array([0.5 , 0.5 , 0.5]) , LPs = np.array([0.5 , 0.5 , 0.5])  , Novs = np.array([0.5 , 0.5 , 0.5])):
+def softmax(values = np.array([0.5]) , PEs = np.array([0.5 , 0.5 , 0.5]) , LPs = np.array([0.5 , 0.5 , 0.5])  , Novs = np.array([0.5 , 0.5 , 0.5])):
     
-    numerator_1 = np.exp(values[0] * PEs[0] + values[1] * LPs[0] + values[2] * Novs[0])
-    numerator_2 = np.exp(values[0] * PEs[1] + values[1] * LPs[1] + values[2] * Novs[1])
-    numerator_3 = np.exp(values[0] * PEs[2] + values[1] * LPs[2] + values[2] * Novs[2])
+    nov_1 = np.exp(- Novs[0])
+    nov_2 = np.exp(- Novs[1])
+    nov_3 = np.exp(- Novs[2])
+    
+    numerator_1 = np.exp(values[0] * PEs[0] + values[1] * LPs[0] + values[2] * nov_1)
+    numerator_2 = np.exp(values[0] * PEs[1] + values[1] * LPs[1] + values[2] * nov_2)
+    numerator_3 = np.exp(values[0] * PEs[2] + values[1] * LPs[2] + values[2] * nov_3)
     
     denom = np.sum([numerator_1 , numerator_2 , numerator_3])
     
@@ -55,9 +60,15 @@ def likelihood(parameter_set, data):
     #Define the variables that are important for the likelihood estimation process
         response = int(df.loc[trial , "Choice"]) - 1 #the stimulus shown on this trial
         
-        PEs = np.array([df.loc[trial , "PE_1"] , df.loc[trial , "PE_2"] , df.loc[trial , "PE_3"]])
-        LPs = np.array([df.loc[trial , "LP_1"] , df.loc[trial , "LP_2"] , df.loc[trial , "LP_3"]])
-        Novs = np.array([df.loc[trial , "Nov_1"] , df.loc[trial , "Nov_2"] , df.loc[trial , "Nov_3"]])
+        if trial == 0: 
+            PEs = np.array([0 , 0 , 0])
+            LPs = np.array([0 , 0 , 0])
+            Novs = np.array([0 , 0 , 0])
+        else: 
+            ##Wacht, maar ik gebruik normaal gezien de vorige trial om te schatten eh, jij ooeennnnnn 
+            PEs = np.array([df.loc[trial - 1 , "PE_1"] , df.loc[trial - 1 , "PE_2"] , df.loc[trial - 1 , "PE_3"]])
+            LPs = np.array([df.loc[trial - 1 , "LP_1"] , df.loc[trial - 1 , "LP_2"] , df.loc[trial - 1 , "LP_3"]])
+            Novs = np.array([df.loc[trial - 1 , "Nov_1"] , df.loc[trial - 1 , "Nov_2"] , df.loc[trial - 1 , "Nov_3"]])
         
         loglikelihoods = np.log(softmax(parameter_set , PEs , LPs , Novs)) 
 
@@ -69,55 +80,73 @@ def likelihood(parameter_set, data):
         
     return -summed_logL
 
-if __name__ == '__main__':
-    #Extract path to data folder
+#Define columns for output file
+column_list = ["Current_PE", "Current_LP" , "Current_Nov", "Estimated_PE", "Estimated_LP" , "Estimated_Nov", "Negative_LogL"]
+estimated_data = pd.DataFrame(columns=column_list)
+
+PE_values = np.random.uniform(-1 , 0 , 1000)
+np.random.shuffle(PE_values)
+LP_values = np.random.uniform(0 , 1 , 1000)
+np.random.shuffle(LP_values)
+Nov_values = np.random.uniform(0 , 1 , 1000)
+np.random.shuffle(Nov_values)
+
+parameter_values = np.column_stack([PE_values , LP_values , Nov_values])
+
+for i in range(parameter_values.shape[0]): 
     
-    directory = "/Users/wardclaeys/Documents/Github/PhD/Parameter_recovery"
-
-    #Get a list of files in the folder and filter on csv files that are not previous fitting results
-    filelist = os.listdir(directory)
-    filtered_filelist = [x for x in filelist if x[-3::]=="csv"]
-    filtered_filelist = [x for x in filtered_filelist if x != "Fitting_results.csv"]
-    filtered_filelist = [x for x in filtered_filelist if x[-13::] != "Simulated.csv"]
-
-    #Go to that directory
-    os.chdir(directory)
-
-    #Define the starting parameters for the likelihood
-    start_params = np.random.uniform(0, 1), np.random.uniform(0 , 1) , np.random.uniform(0 , 1)
-
-    #Define columns for output file
-    column_list = ["Current_PE", "Current_LP" , "Current_Nov", "Estimated_PE", "Estimated_LP" , "Estimated_Nov", "Negative_LogL"]
-    estimated_data = pd.DataFrame(columns=column_list)
+    weight_PE , weight_LP , weight_Nov = parameter_values[i , : ]
     
-    idx = -1
-    for file in filtered_filelist:
-        idx +=1
-
-        #Minimize negative loglikelihood
-        df = pd.read_csv("Check_this_file.csv") 
-        #optimization_output = optimize.minimize(fun = likelihood , x0 = [start_params, df]) #, options = {'maxfev':10000, 'xatol':0.00001, 'return_all':0})
+    generate_dataset(n = 200 , weight_PE = weight_PE , weight_LP = weight_LP, weight_Nov = weight_Nov)
+    
+    if __name__ == '__main__':
+        #Extract path to data folder
         
-        optimization_output = optimize.minimize(fun = likelihood , x0 = start_params , args = df , options = {'maxfev':10000, 'xatol':0.00001, 'return_all':0})
+        directory = "/Users/wardclaeys/Documents/Github/PhD/Parameter_recovery"
+    
+        #Get a list of files in the folder and filter on csv files that are not previous fitting results
+        filelist = os.listdir(directory)
+        filtered_filelist = [x for x in filelist if x[-3::]=="csv"]
+        filtered_filelist = [x for x in filtered_filelist if x != "Fitting_results.csv"]
+        filtered_filelist = [x for x in filtered_filelist if x[-13::] != "Simulated.csv"]
+    
+        #Go to that directory
+        os.chdir(directory)
+    
+        #Define the starting parameters for the likelihood
+        start_params = np.random.uniform(0 , 1) , np.random.uniform(0 , 1) , np.random.uniform(0 , 1)
         
-        #Get minimum log likelihood and parameter estimations
-        LL = optimization_output['fun']
-        estimated_parameters = optimization_output['x']
-        
-        #print("estimated learning rate is: {0} and estimated inverse temperature is: {1}.\n\n".format(lr, inv_temp))
-        #Store everything in output file
-        estimated_data.loc[idx , ["Current_PE", "Current_LP" , "Current_Nov"]] = [df.loc[0 , "weight_PE"] , df.loc[0 , "weight_LP"] , df.loc[0 , "weight_Nov"]]
-        estimated_data.loc[idx , ["Estimated_PE", "Estimated_LP" , "Estimated_Nov", "Negative_LogL"]] = [estimated_parameters[0] , estimated_parameters[1] , estimated_parameters[2] , LL]
-        
-        print("Simulated data")
-
-    #Write results of parameter fitting
+        idx = -1
+        for file in filtered_filelist:
+            idx +=1
+    
+            #Minimize negative loglikelihood
+            df = pd.read_csv("Check_this_file.csv") 
+            #optimization_output = optimize.minimize(fun = likelihood , x0 = [start_params, df]) #, options = {'maxfev':10000, 'xatol':0.00001, 'return_all':0})
+            
+            optimization_output = optimize.minimize(fun = likelihood , x0 = start_params , args = df , options = {'maxfev':10000, 'xatol':0.00001, 'return_all':0})
+            
+            #Get minimum log likelihood and parameter estimations
+            LL = optimization_output['fun']
+            estimated_parameters = optimization_output['x']
+            
+            #print("estimated learning rate is: {0} and estimated inverse temperature is: {1}.\n\n".format(lr, inv_temp))
+            #Store everything in output file
+            estimated_data.loc[i , ["Current_PE", "Current_LP" , "Current_Nov"]] = [df.loc[0 , "weight_PE"] , df.loc[0 , "weight_LP"] , df.loc[0 , "weight_Nov"]]
+            #estimated_data.loc[idx , ["Estimated_PE", "Estimated_LP" , "Estimated_Nov", "Negative_LogL"]] = [estimated_parameters[0] , estimated_parameters[1] , estimated_parameters[2] , LL]
+            estimated_data.loc[i , ["Estimated_PE" , "Estimated_LP" , "Estimated_Nov"]] = [estimated_parameters[0] , estimated_parameters[1] , estimated_parameters[2]]
+            
+            print("Simulated data")
+    
+        #Write results of parameter fitting
     estimated_data.to_csv("Fitting_results.csv", columns = column_list, float_format ='%.3f')
     print("End of fitting procedure")
 
+mean_test = pd.read_csv("Fitting_results.csv") 
 
-
-
+print(np.mean(mean_test["Estimated_PE"]))
+print(np.mean(mean_test["Estimated_LP"]))
+print(np.mean(mean_test["Estimated_Nov"]))
 
 
 
--- Parameter_recovery/Parameter_recovery.py
+++ Parameter_recovery/Parameter_recovery.py
@@ -45,154 +45,158 @@ def softmax(x):
     
     return probabilities
 
-##Initialize the probabilities of selecting a task, just based on the probability to be correct 
-#We'll update these probabilities every time a task is selected
-initialize_probabilities = [0.5 , 0.25 , 0.125]
 
-data = pd.DataFrame()
-
-data.loc[0 , "Trial_nr"] = 0
-data.loc[0 , "Choice"] = 0
-data.loc[0 , "Reward"] = 0
-data.loc[0 , ["probability_1" , "probability_2" , "probability_3"]] = initialize_probabilities
-data.loc[0 , ["PE_1" , "PE_2" , "PE_3"]] = [0 , 0 , 0]
-data.loc[0 , ["LP_1" , "LP_2" , "LP_3"]] = [0 , 0 , 0]
-data.loc[0 , ["Nov_1" , "Nov_2" , "Nov_3"]] = [0 , 0 , 0]
-task_options = [1 , 2 , 3]
-
-PE_1 , PE_2 , PE_3 = 0 , 0 , 0
-novelty_1 , novelty_2 , novelty_3 = 0 , 0 , 0
-
-weight_PE = -0.5
-weight_LP = 0.5 
-weight_Nov = 0.5
-
-data.loc[ : , ["weight_PE" , "weight_LP" , "weight_Nov"]] = [weight_PE , weight_LP , weight_Nov]
-
-lambd = 0.05
-
-##Juist of fout erbij doen nog 
-
-for i in range(2000): 
+def generate_dataset(n = 200 , weight_PE = -0.5 , weight_LP = 0.5 , weight_Nov = 0.5): 
+    ##Initialize the probabilities of selecting a task, just based on the probability to be correct 
+    #We'll update these probabilities every time a task is selected
+    initialize_probabilities = [0.5 , 0.25 , 0.125]
     
-    data.loc[i , "Trial_nr"] = i
+    data = pd.DataFrame()
     
-    if i == 0: 
-        task_choice = np.random.choice(task_options)
-        
-        data.loc[i , ["model_1_probability" , "model_2_probability" , "model_3_probability"]]= [0.33 , 0.33 , 0.33]
-        
-    else: 
-        
-        novelty_1_adjusted = np.exp(- novelty_1)
-        novelty_2_adjusted = np.exp(- novelty_2)
-        novelty_3_adjusted = np.exp(- novelty_3)
-        
-        numerator_1 = np.exp(weight_PE * data.loc[i - 1 , "PE_1"] + weight_LP * data.loc[i - 1 , "LP_1"] + weight_Nov * novelty_1_adjusted) 
-        numerator_2 = np.exp(weight_PE * data.loc[i - 1 , "PE_2"] + weight_LP * data.loc[i - 1 , "LP_2"] + weight_Nov * novelty_2_adjusted) 
-        numerator_3 = np.exp(weight_PE * data.loc[i - 1 , "PE_3"] + weight_LP * data.loc[i - 1 , "LP_3"] + weight_Nov * novelty_3_adjusted) 
+    data.loc[0 , "Trial_nr"] = 0
+    data.loc[0 , "Choice"] = 0
+    data.loc[0 , "Reward"] = 0
+    data.loc[0 , ["probability_1" , "probability_2" , "probability_3"]] = initialize_probabilities
+    data.loc[0 , ["PE_1" , "PE_2" , "PE_3"]] = [0 , 0 , 0]
+    data.loc[0 , ["LP_1" , "LP_2" , "LP_3"]] = [0 , 0 , 0]
+    data.loc[0 , ["Nov_1" , "Nov_2" , "Nov_3"]] = [0 , 0 , 0]
+    task_options = [1 , 2 , 3]
     
-        model_1 = numerator_1 / np.sum([numerator_1 , numerator_2 , numerator_3])
-        model_2 = numerator_2 / np.sum([numerator_1 , numerator_2 , numerator_3])
-        model_3 = numerator_3 / np.sum([numerator_1 , numerator_2 , numerator_3])
-        
-        task_choice = np.random.choice(task_options , p = [model_1 , model_2 , model_3])
-        
-        data.loc[i , ["model_1_probability" , "model_2_probability" , "model_3_probability"]]= [model_1 , model_2 , model_3]
-        
-    task_probabilities = list(data.loc[i , ["probability_1" , "probability_2" , "probability_3"]])
+    PE_1 , PE_2 , PE_3 = 0 , 0 , 0
+    novelty_1 , novelty_2 , novelty_3 = 0 , 0 , 0
     
-    data.loc[i , "Choice"] = task_choice
+#    weight_PE = -0.5
+#    weight_LP = 0.5 
+#    weight_Nov = 0.5
     
-    if task_choice == 1: 
-        prob_correct = data.loc[i , "probability_1"]
-        correct = np.random.choice([1 , 0] , p = [prob_correct , 1 - prob_correct])
+    data.loc[ : , ["weight_PE" , "weight_LP" , "weight_Nov"]] = [weight_PE , weight_LP , weight_Nov]
+    
+    lambd = 0.05
+    
+    ##Juist of fout erbij doen nog 
+    
+    for i in range(n): 
         
-        PE_1 = correct - data.loc[i , "probability_1"]
+        data.loc[i , "Trial_nr"] = i
         
         if i == 0: 
-            PE_2 , PE_3 = 0 , 0
-            LP_1 , LP_2 , LP_3 = 0 , 0 , 0
-        else: 
-            PE_2 = data.loc[i - 1 , "PE_2"]
-            PE_3 = data.loc[i - 1 , "PE_3"]
+            task_choice = np.random.choice(task_options)
             
-            LP_1 = data.loc[i - 1 , "PE_1"] - PE_1
-            LP_2 = data.loc[i - 1 , "PE_2"]
-            LP_3 = data.loc[i - 1 , "PE_3"]
-        
-        #Update the probability of being correct by adding lambda * (1 - p(t - 1))
-        task_probabilities[0] = data.loc[i , "probability_1"] + lambd * (1 - data.loc[i , "probability_1"])
-        #The not chosen probabilities remain the same
-        task_probabilities[1] = data.loc[i , "probability_2"]
-        task_probabilities[2] = data.loc[i , "probability_3"]
-        
-        novelty_1 += 1
-        novelty_2 = 0
-        novelty_3 = 0
-
-    elif task_choice == 2: 
-        prob_correct = data.loc[i , "probability_2"]
-        correct = np.random.choice([1 , 0] , p = [prob_correct , 1 - prob_correct])
-        
-        PE_2 = correct - data.loc[i , "probability_2"]
-        
-        if i == 0: 
-            PE_1 , PE_3 = 0 , 0
-            LP_1 , LP_2 , LP_3 = 0 , 0 , 0
-        else: 
-            PE_1 = data.loc[i - 1 , "PE_1"]
-            PE_3 = data.loc[i - 1 , "PE_3"]
+            data.loc[i , ["model_1_probability" , "model_2_probability" , "model_3_probability"]]= [0.33 , 0.33 , 0.33]
             
-            LP_2 = data.loc[i - 1 , "PE_2"] - PE_2
-            LP_1 = data.loc[i - 1 , "PE_1"]
-            LP_3 = data.loc[i - 1 , "PE_3"]
-        
-        #Update the probability of being correct by adding lambda * (1 - p(t - 1))
-        task_probabilities[1] = data.loc[i , "probability_2"] + lambd * (1 - data.loc[i , "probability_2"])
-        #The not chosen probabilities remain the same
-        task_probabilities[0] = data.loc[i , "probability_1"]
-        task_probabilities[2] = data.loc[i , "probability_3"]
-        
-        novelty_2 += 1
-        novelty_1 = 0
-        novelty_3 = 0
-        
-    else: 
-        prob_correct = data.loc[i , "probability_3"]
-        correct = np.random.choice([1 , 0] , p = [prob_correct , 1 - prob_correct])
-        
-        PE_3 = correct - data.loc[i , "probability_3"]
-        
-        if i == 0: 
-            PE_1 , PE_2 = 0 , 0
-            LP_1 , LP_2 , LP_3 = 0 , 0 , 0
         else: 
-            PE_1 = data.loc[i - 1 , "PE_1"]
-            PE_2 = data.loc[i - 1 , "PE_2"]
             
-            LP_3 = data.loc[i - 1 , "PE_3"] - PE_3
-            LP_1 = data.loc[i - 1 , "PE_1"]
-            LP_2 = data.loc[i - 1 , "PE_2"]
+            novelty_1_adjusted = np.exp(- novelty_1)
+            novelty_2_adjusted = np.exp(- novelty_2)
+            novelty_3_adjusted = np.exp(- novelty_3)
+            
+            numerator_1 = np.exp(weight_PE * data.loc[i - 1 , "PE_1"] + weight_LP * data.loc[i - 1 , "LP_1"] + weight_Nov * novelty_1_adjusted) 
+            numerator_2 = np.exp(weight_PE * data.loc[i - 1 , "PE_2"] + weight_LP * data.loc[i - 1 , "LP_2"] + weight_Nov * novelty_2_adjusted) 
+            numerator_3 = np.exp(weight_PE * data.loc[i - 1 , "PE_3"] + weight_LP * data.loc[i - 1 , "LP_3"] + weight_Nov * novelty_3_adjusted) 
         
-        #Update the probability of being correct by adding lambda * (1 - p(t - 1))
-        task_probabilities[2] = data.loc[i , "probability_3"] + lambd * (1 - data.loc[i , "probability_3"])
-        #The not chosen probabilities remain the same
-        task_probabilities[0] = data.loc[i , "probability_1"]
-        task_probabilities[1] = data.loc[i , "probability_2"]
+            model_1 = numerator_1 / np.sum([numerator_1 , numerator_2 , numerator_3])
+            model_2 = numerator_2 / np.sum([numerator_1 , numerator_2 , numerator_3])
+            model_3 = numerator_3 / np.sum([numerator_1 , numerator_2 , numerator_3])
+            
+            task_choice = np.random.choice(task_options , p = [model_1 , model_2 , model_3])
+            
+            data.loc[i , ["model_1_probability" , "model_2_probability" , "model_3_probability"]]= [model_1 , model_2 , model_3]
+            
+        task_probabilities = list(data.loc[i , ["probability_1" , "probability_2" , "probability_3"]])
         
-        novelty_3 += 1
-        novelty_1 = 0
-        novelty_2 = 0
+        data.loc[i , "Choice"] = task_choice
+        
+        if task_choice == 1: 
+            prob_correct = data.loc[i , "probability_1"]
+            correct = np.random.choice([1 , 0] , p = [prob_correct , 1 - prob_correct])
+            
+            PE_1 = correct - data.loc[i , "probability_1"]
+            
+            if i == 0: 
+                PE_2 , PE_3 = 0 , 0
+                LP_1 , LP_2 , LP_3 = 0 , 0 , 0
+            else: 
+                PE_2 = data.loc[i - 1 , "PE_2"]
+                PE_3 = data.loc[i - 1 , "PE_3"]
+                
+                LP_1 = data.loc[i - 1 , "PE_1"] - PE_1
+                LP_2 = data.loc[i - 1 , "PE_2"]
+                LP_3 = data.loc[i - 1 , "PE_3"]
+            
+            #Update the probability of being correct by adding lambda * (1 - p(t - 1))
+            task_probabilities[0] = data.loc[i , "probability_1"] + lambd * (1 - data.loc[i , "probability_1"])
+            #The not chosen probabilities remain the same
+            task_probabilities[1] = data.loc[i , "probability_2"]
+            task_probabilities[2] = data.loc[i , "probability_3"]
+            
+            novelty_1 += 1
+            novelty_2 = 0
+            novelty_3 = 0
     
-    if i != 1999: 
-        data.loc[i + 1 , ["probability_1" , "probability_2" , "probability_3"]] = task_probabilities
-    data.loc[i , ["PE_1" , "PE_2" , "PE_3"]] = [PE_1 , PE_2 , PE_3]
-    data.loc[i , ["LP_1" , "LP_2" , "LP_3"]] = [LP_1 , LP_2 , LP_3]
-    data.loc[i , ["Nov_1" , "Nov_2" , "Nov_3"]] = [novelty_1 , novelty_2 , novelty_3]
-    data.loc[i , "Reward"] = correct 
-
-data.to_csv("Check_this_file.csv")
+        elif task_choice == 2: 
+            prob_correct = data.loc[i , "probability_2"]
+            correct = np.random.choice([1 , 0] , p = [prob_correct , 1 - prob_correct])
+            
+            PE_2 = correct - data.loc[i , "probability_2"]
+            
+            if i == 0: 
+                PE_1 , PE_3 = 0 , 0
+                LP_1 , LP_2 , LP_3 = 0 , 0 , 0
+            else: 
+                PE_1 = data.loc[i - 1 , "PE_1"]
+                PE_3 = data.loc[i - 1 , "PE_3"]
+                
+                LP_2 = data.loc[i - 1 , "PE_2"] - PE_2
+                LP_1 = data.loc[i - 1 , "PE_1"]
+                LP_3 = data.loc[i - 1 , "PE_3"]
+            
+            #Update the probability of being correct by adding lambda * (1 - p(t - 1))
+            task_probabilities[1] = data.loc[i , "probability_2"] + lambd * (1 - data.loc[i , "probability_2"])
+            #The not chosen probabilities remain the same
+            task_probabilities[0] = data.loc[i , "probability_1"]
+            task_probabilities[2] = data.loc[i , "probability_3"]
+            
+            novelty_2 += 1
+            novelty_1 = 0
+            novelty_3 = 0
+            
+        else: 
+            prob_correct = data.loc[i , "probability_3"]
+            correct = np.random.choice([1 , 0] , p = [prob_correct , 1 - prob_correct])
+            
+            PE_3 = correct - data.loc[i , "probability_3"]
+            
+            if i == 0: 
+                PE_1 , PE_2 = 0 , 0
+                LP_1 , LP_2 , LP_3 = 0 , 0 , 0
+            else: 
+                PE_1 = data.loc[i - 1 , "PE_1"]
+                PE_2 = data.loc[i - 1 , "PE_2"]
+                
+                LP_3 = data.loc[i - 1 , "PE_3"] - PE_3
+                LP_1 = data.loc[i - 1 , "PE_1"]
+                LP_2 = data.loc[i - 1 , "PE_2"]
+            
+            #Update the probability of being correct by adding lambda * (1 - p(t - 1))
+            task_probabilities[2] = data.loc[i , "probability_3"] + lambd * (1 - data.loc[i , "probability_3"])
+            #The not chosen probabilities remain the same
+            task_probabilities[0] = data.loc[i , "probability_1"]
+            task_probabilities[1] = data.loc[i , "probability_2"]
+            
+            novelty_3 += 1
+            novelty_1 = 0
+            novelty_2 = 0
+        
+        if i != (n - 1): 
+            data.loc[i + 1 , ["probability_1" , "probability_2" , "probability_3"]] = task_probabilities
+        data.loc[i , ["PE_1" , "PE_2" , "PE_3"]] = [PE_1 , PE_2 , PE_3]
+        data.loc[i , ["LP_1" , "LP_2" , "LP_3"]] = [LP_1 , LP_2 , LP_3]
+        data.loc[i , ["Nov_1" , "Nov_2" , "Nov_3"]] = [novelty_1 , novelty_2 , novelty_3]
+        data.loc[i , "Reward"] = correct 
+    
+    data.to_csv("Check_this_file.csv")
+    
+    return 
 
 
 
